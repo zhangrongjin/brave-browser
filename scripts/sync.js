940d690d7239bed2e635f6acd78ab5087c3a5cc8
bridiver <github@brianjohnson.cc>
2018-11-13 11:47:43 -0700

    add android support

--- scripts/sync.js
+++ scripts/sync.js
@@ -4,8 +4,6 @@ const fs = require('fs-extra')
 const config = require('../lib/config')
 const util = require('../lib/util')
 
-const projectNames = config.projectNames.filter((project) => config.projects[project].ref)
-
 program
   .version(process.env.npm_package_version)
   .option('--gclient_file <file>', 'gclient config file location')
@@ -15,10 +13,7 @@ program
   .option('--submodule_sync', 'run submodule sync')
   .option('--init', 'initialize all dependencies')
   .option('--all', 'update all projects')
-projectNames.forEach((name) => {
-  let project = config.projects[name]
-  program.option('--' + project.arg_name + '_ref <ref>', name + ' ref to checkout')
-})
+  .option('--android', 'initialize/update android browser')
 
 program.parse(process.argv)
 config.update(program)
